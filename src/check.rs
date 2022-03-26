a0af0bd3a8ea53c2bbd4b72ac8009264003b4a14
JoJoJet <joe102000@gmail.com>
2022-03-26 15:16:05 -0400

    make checking code internally generic

---
+++ src/check.rs
@@ -0,0 +1,338 @@
+use std::{fmt, marker::PhantomData};
+
+use crate::{unwrap_display, IntoInner, ToOrd};
+
+/// whether or not to panic on NaN.
+pub(crate) const STRICT: bool = cfg!(any(debug_assertions, feature = "strict"));
+
+pub(crate) trait Check<F> {
+    type Error: fmt::Display;
+    fn check(_: F) -> Result<F, Self::Error>;
+}
+
+#[repr(transparent)]
+pub(crate) struct Checked<F, C: Check<F>>(F, PhantomData<C>);
+
+macro_rules! checked {
+    ($val: expr) => {
+        Checked($val, PhantomData)
+    };
+}
+
+impl<F: Copy, C: Check<F>> Checked<F, C> {
+    pub fn try_new(val: F) -> Result<Self, C::Error> {
+        // This is not a TryFrom implementation due to [this issue](https://github.com/rust-lang/rust/issues/50133).
+        let val = C::check(val)?;
+        Ok(checked!(val))
+    }
+    #[track_caller]
+    pub fn new(val: F) -> Self {
+        if STRICT {
+            unwrap_display(Self::try_new(val))
+        } else {
+            checked!(val)
+        }
+    }
+    pub fn val(self) -> F {
+        self.0
+    }
+}
+
+impl<F, C: Check<F>> IntoInner<F> for Checked<F, C> {
+    #[inline]
+    fn into_inner(self) -> F {
+        self.0
+    }
+}
+
+impl<F: fmt::Debug, C: Check<F>> fmt::Debug for Checked<F, C> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Debug::fmt(&self.0, f)
+    }
+}
+impl<F: fmt::Display, C: Check<F>> fmt::Display for Checked<F, C> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Display::fmt(&self.0, f)
+    }
+}
+
+impl<F: Clone, C: Check<F>> Clone for Checked<F, C> {
+    #[inline]
+    fn clone(&self) -> Self {
+        checked!(self.0.clone())
+    }
+    #[inline]
+    fn clone_from(&mut self, other: &Self) {
+        self.0.clone_from(&other.0);
+    }
+}
+impl<F: Copy, C: Check<F>> Copy for Checked<F, C> {}
+
+impl<F: Default, C: Check<F>> Default for Checked<F, C> {
+    #[inline]
+    fn default() -> Self {
+        checked!(F::default())
+    }
+}
+
+use std::cmp::{Eq, PartialEq};
+impl<F: ToOrd, C: Check<F>, Rhs: IntoInner<F> + Copy> PartialEq<Rhs> for Checked<F, C> {
+    fn eq(&self, rhs: &Rhs) -> bool {
+        let rhs: F = (*rhs).into_inner();
+        self.0.total_eq(rhs)
+    }
+}
+impl<F: ToOrd, C: Check<F>> Eq for Checked<F, C> {}
+
+use std::cmp::{Ord, Ordering, PartialOrd};
+impl<F: ToOrd, C: Check<F>> PartialOrd<F> for Checked<F, C> {
+    fn partial_cmp(&self, rhs: &F) -> Option<Ordering> {
+        let rhs = C::check(*rhs).ok()?.to_ord();
+        let lhs = self.0.to_ord();
+        Some(lhs.cmp(&rhs))
+    }
+}
+impl<F: ToOrd, C: Check<F>> PartialOrd for Checked<F, C> {
+    fn partial_cmp(&self, rhs: &Self) -> Option<Ordering> {
+        Some(self.cmp(rhs))
+    }
+}
+impl<F: ToOrd, C: Check<F>> Ord for Checked<F, C> {
+    fn cmp(&self, rhs: &Self) -> Ordering {
+        let lhs = self.0.to_ord();
+        let rhs = rhs.0.to_ord();
+        lhs.cmp(&rhs)
+    }
+}
+
+use std::ops::{Add, Neg, Sub};
+impl<F: Copy, C: Check<F>> Checked<F, C> {
+    pub fn try_add(self, rhs: impl IntoInner<F>) -> Result<Self, C::Error>
+    where
+        F: Add<Output = F>,
+    {
+        let output = self.0 + rhs.into_inner();
+        Self::try_new(output)
+    }
+    pub fn try_sub(self, rhs: impl IntoInner<F>) -> Result<Self, C::Error>
+    where
+        F: Sub<Output = F>,
+    {
+        let output = self.0 - rhs.into_inner();
+        Self::try_new(output)
+    }
+    pub fn try_neg(self) -> Result<Self, C::Error>
+    where
+        F: Neg<Output = F>,
+    {
+        let output = -self.0;
+        Self::try_new(output)
+    }
+}
+impl<F: Add<Output = F> + Copy, C: Check<F>, Rhs: IntoInner<F>> Add<Rhs> for Checked<F, C> {
+    type Output = Self;
+    #[track_caller]
+    fn add(self, rhs: Rhs) -> Self::Output {
+        let rhs = rhs.into_inner();
+        if STRICT {
+            unwrap_display(self.try_add(rhs))
+        } else {
+            checked!(self.0 + rhs)
+        }
+    }
+}
+impl<F: Sub<Output = F> + Copy, C: Check<F>, Rhs: IntoInner<F>> Sub<Rhs> for Checked<F, C> {
+    type Output = Self;
+    #[track_caller]
+    fn sub(self, rhs: Rhs) -> Self::Output {
+        let rhs = rhs.into_inner();
+        if STRICT {
+            unwrap_display(self.try_sub(rhs))
+        } else {
+            checked!(self.0 - rhs)
+        }
+    }
+}
+impl<F: Neg<Output = F> + Copy, C: Check<F>> Neg for Checked<F, C> {
+    type Output = Self;
+    #[track_caller]
+    fn neg(self) -> Self::Output {
+        if STRICT {
+            unwrap_display(self.try_neg())
+        } else {
+            checked!(-self.0)
+        }
+    }
+}
+
+use std::ops::{Div, Mul, Rem};
+impl<F: Copy, C: Check<F>> Checked<F, C> {
+    pub fn try_mul(self, rhs: impl IntoInner<F>) -> Result<Self, C::Error>
+    where
+        F: Mul<Output = F>,
+    {
+        let output = self.0 * rhs.into_inner();
+        Self::try_new(output)
+    }
+    pub fn try_div(self, rhs: impl IntoInner<F>) -> Result<Self, C::Error>
+    where
+        F: Div<Output = F>,
+    {
+        let output = self.0 / rhs.into_inner();
+        Self::try_new(output)
+    }
+    pub fn try_rem(self, rhs: impl IntoInner<F>) -> Result<Self, C::Error>
+    where
+        F: Rem<Output = F>,
+    {
+        let output = self.0 % rhs.into_inner();
+        Self::try_new(output)
+    }
+}
+impl<F: Mul<Output = F> + Copy, C: Check<F>, Rhs: IntoInner<F>> Mul<Rhs> for Checked<F, C> {
+    type Output = Self;
+    #[track_caller]
+    fn mul(self, rhs: Rhs) -> Self::Output {
+        let rhs = rhs.into_inner();
+        if STRICT {
+            unwrap_display(self.try_mul(rhs))
+        } else {
+            checked!(self.0 * rhs)
+        }
+    }
+}
+impl<F: Div<Output = F> + Copy, C: Check<F>, Rhs: IntoInner<F>> Div<Rhs> for Checked<F, C> {
+    type Output = Self;
+    #[track_caller]
+    fn div(self, rhs: Rhs) -> Self::Output {
+        let rhs = rhs.into_inner();
+        if STRICT {
+            unwrap_display(self.try_div(rhs))
+        } else {
+            checked!(self.0 / rhs)
+        }
+    }
+}
+impl<F: Rem<Output = F> + Copy, C: Check<F>, Rhs: IntoInner<F>> Rem<Rhs> for Checked<F, C> {
+    type Output = Self;
+    #[track_caller]
+    fn rem(self, rhs: Rhs) -> Self::Output {
+        let rhs = rhs.into_inner();
+        if STRICT {
+            unwrap_display(self.try_rem(rhs))
+        } else {
+            checked!(self.0 % rhs)
+        }
+    }
+}
+
+use crate::Pow;
+impl<F: Copy + Pow, C: Check<F>> Checked<F, C> {
+    pub fn try_powf(self, n: impl IntoInner<F>) -> Result<Self, C::Error> {
+        let output = self.0.powf(n.into_inner());
+        Self::try_new(output)
+    }
+    pub fn try_powi(self, n: i32) -> Result<Self, C::Error> {
+        let output = self.0.powi(n);
+        Self::try_new(output)
+    }
+    #[track_caller]
+    pub fn powf(self, n: impl IntoInner<F>) -> Self {
+        let n = n.into_inner();
+        if STRICT {
+            unwrap_display(self.try_powf(n))
+        } else {
+            checked!(self.0.powf(n))
+        }
+    }
+    #[track_caller]
+    pub fn powi(self, n: i32) -> Self {
+        if STRICT {
+            unwrap_display(self.try_powi(n))
+        } else {
+            checked!(self.0.powi(n))
+        }
+    }
+}
+
+use crate::Root;
+impl<F: Copy + Root, C: Check<F>> Checked<F, C> {
+    pub fn try_sqrt(self) -> Result<Self, C::Error> {
+        let output = self.0.sqrt();
+        Self::try_new(output)
+    }
+    pub fn try_cbrt(self) -> Result<Self, C::Error> {
+        let output = self.0.cbrt();
+        Self::try_new(output)
+    }
+    #[track_caller]
+    pub fn sqrt(self) -> Self {
+        if STRICT {
+            unwrap_display(self.try_sqrt())
+        } else {
+            checked!(self.0.sqrt())
+        }
+    }
+    #[track_caller]
+    pub fn cbrt(self) -> Self {
+        if STRICT {
+            unwrap_display(self.try_cbrt())
+        } else {
+            checked!(self.0.cbrt())
+        }
+    }
+}
+
+use crate::Log;
+impl<F: Copy + Log, C: Check<F>> Checked<F, C> {
+    pub fn try_log(self, b: impl IntoInner<F>) -> Result<Self, C::Error> {
+        let output = self.0.log(b.into_inner());
+        Self::try_new(output)
+    }
+    pub fn try_ln(self) -> Result<Self, C::Error> {
+        let output = self.0.ln();
+        Self::try_new(output)
+    }
+    pub fn try_log2(self) -> Result<Self, C::Error> {
+        let output = self.0.log2();
+        Self::try_new(output)
+    }
+    pub fn try_log10(self) -> Result<Self, C::Error> {
+        let output = self.0.log10();
+        Self::try_new(output)
+    }
+
+    #[track_caller]
+    pub fn log(self, base: impl IntoInner<F>) -> Self {
+        let base = base.into_inner();
+        if STRICT {
+            unwrap_display(self.try_log(base))
+        } else {
+            checked!(self.0.log(base))
+        }
+    }
+    #[track_caller]
+    pub fn ln(self) -> Self {
+        if STRICT {
+            unwrap_display(self.try_ln())
+        } else {
+            checked!(self.0.ln())
+        }
+    }
+    #[track_caller]
+    pub fn log2(self) -> Self {
+        if STRICT {
+            unwrap_display(self.try_log2())
+        } else {
+            checked!(self.0.log2())
+        }
+    }
+    #[track_caller]
+    pub fn log10(self) -> Self {
+        if STRICT {
+            unwrap_display(self.try_log10())
+        } else {
+            checked!(self.0.log10())
+        }
+    }
+}
---
+++ src/real.rs
@@ -0,0 +1,358 @@
+use crate::{check::Checked, IntoInner, IsNan};
+
+/// The error produced when NaN is encountered.
+#[derive(Debug, Clone, Copy)]
+pub struct NanError;
+impl std::fmt::Display for NanError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "encountered NaN unexpectedly")
+    }
+}
+
+struct NanCheck;
+impl<F: IsNan> crate::check::Check<F> for NanCheck {
+    type Error = NanError;
+    fn check(val: F) -> Result<F, NanError> {
+        if val.is_nan() {
+            Err(NanError)
+        } else {
+            Ok(val)
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Default)]
+#[repr(transparent)]
+pub struct Real<F: IsNan>(Checked<F, NanCheck>);
+
+impl<F: IsNan> Real<F> {
+    /// Creates a new [`Real`] float, panicking if it's NaN.  
+    ///
+    /// Note that this function will *not* panic in release mode,
+    /// unless the `strict` feature flag is set.
+    #[track_caller]
+    pub fn new(val: F) -> Self {
+        Self(Checked::new(val))
+    }
+    /// Attempts to create a new [`Real`] float.
+    /// # Errors
+    /// If the number is NaN.
+    pub fn try_new(val: F) -> Result<Self, NanError> {
+        // This is not a TryFrom implementation due to [this issue](https://github.com/rust-lang/rust/issues/50133).
+        Checked::try_new(val).map(Self)
+    }
+    /// Gets the inner value of this number.
+    pub fn val(self) -> F {
+        self.0.val()
+    }
+}
+
+impl<F: IsNan> IntoInner<F> for Real<F> {
+    #[inline]
+    fn into_inner(self) -> F {
+        self.0.into_inner()
+    }
+}
+
+use crate::ToOrd;
+use std::cmp::{Eq, PartialEq};
+impl<F: IsNan + ToOrd, Rhs: IntoInner<F> + Copy> PartialEq<Rhs> for Real<F> {
+    fn eq(&self, rhs: &Rhs) -> bool {
+        self.0 == *rhs
+    }
+}
+impl<F: IsNan + ToOrd> Eq for Real<F> {}
+
+use std::cmp::{Ord, Ordering, PartialOrd};
+impl<F: IsNan + ToOrd> PartialOrd<F> for Real<F> {
+    fn partial_cmp(&self, rhs: &F) -> Option<Ordering> {
+        self.0.partial_cmp(rhs)
+    }
+}
+impl<F: IsNan + ToOrd> PartialOrd for Real<F> {
+    fn partial_cmp(&self, rhs: &Self) -> Option<Ordering> {
+        self.0.partial_cmp(&rhs.0)
+    }
+}
+impl<F: IsNan + ToOrd> Ord for Real<F> {
+    fn cmp(&self, rhs: &Self) -> Ordering {
+        self.0.cmp(&rhs.0)
+    }
+}
+
+use std::ops::{Add, Neg, Sub};
+impl<F: IsNan> Real<F> {
+    /// Attempts to add two numbers.
+    /// # Errors
+    /// If the result is NaN.
+    pub fn try_add(self, rhs: impl IntoInner<F>) -> Result<Self, NanError>
+    where
+        F: Add<Output = F>,
+    {
+        self.0.try_add(rhs.into_inner()).map(Self)
+    }
+    /// Attempts to subtract two numbers.
+    /// # Errors
+    /// If the result is NaN.
+    pub fn try_sub(self, rhs: impl IntoInner<F>) -> Result<Self, NanError>
+    where
+        F: Sub<Output = F>,
+    {
+        self.0.try_sub(rhs.into_inner()).map(Self)
+    }
+}
+impl<F: Add<Output = F> + IsNan, Rhs: IntoInner<F>> Add<Rhs> for Real<F> {
+    type Output = Self;
+    #[track_caller]
+    fn add(self, rhs: Rhs) -> Self::Output {
+        Self(self.0 + rhs.into_inner())
+    }
+}
+impl<F: Sub<Output = F> + IsNan, Rhs: IntoInner<F>> Sub<Rhs> for Real<F> {
+    type Output = Self;
+    #[track_caller]
+    fn sub(self, rhs: Rhs) -> Self::Output {
+        Self(self.0 - rhs.into_inner())
+    }
+}
+impl<F: Neg<Output = F> + IsNan> Neg for Real<F> {
+    type Output = Self;
+    fn neg(self) -> Self::Output {
+        Self(-self.0)
+    }
+}
+
+use std::ops::{Div, Mul, Rem};
+impl<F: IsNan> Real<F> {
+    /// Attempts to multiply two numbers.
+    /// # Errors
+    /// If the result is NaN.
+    pub fn try_mul(self, rhs: impl IntoInner<F>) -> Result<Self, NanError>
+    where
+        F: Mul<Output = F>,
+    {
+        self.0.try_mul(rhs.into_inner()).map(Self)
+    }
+    /// Attempts to divide self by another number.
+    /// # Errors
+    /// If the result is NaN.
+    pub fn try_div(self, rhs: impl IntoInner<F>) -> Result<Self, NanError>
+    where
+        F: Div<Output = F>,
+    {
+        self.0.try_div(rhs.into_inner()).map(Self)
+    }
+    /// Attempts to find the remainder of `self / rhs`.
+    /// # Errors
+    /// If the result is NaN.
+    pub fn try_rem(self, rhs: impl IntoInner<F>) -> Result<Self, NanError>
+    where
+        F: Rem<Output = F>,
+    {
+        self.0.try_rem(rhs.into_inner()).map(Self)
+    }
+}
+impl<F: Mul<Output = F> + IsNan, Rhs: IntoInner<F>> Mul<Rhs> for Real<F> {
+    type Output = Self;
+    #[track_caller]
+    fn mul(self, rhs: Rhs) -> Self::Output {
+        Self(self.0 * rhs.into_inner())
+    }
+}
+impl<F: Div<Output = F> + IsNan, Rhs: IntoInner<F>> Div<Rhs> for Real<F> {
+    type Output = Self;
+    #[track_caller]
+    fn div(self, rhs: Rhs) -> Self::Output {
+        Self(self.0 / rhs.into_inner())
+    }
+}
+impl<F: Rem<Output = F> + IsNan, Rhs: IntoInner<F>> Rem<Rhs> for Real<F> {
+    type Output = Self;
+    #[track_caller]
+    fn rem(self, rhs: Rhs) -> Self::Output {
+        Self(self.0 % rhs.into_inner())
+    }
+}
+
+use crate::Pow;
+impl<F: IsNan + Pow> Real<F> {
+    /// Attempts to raise `self` to the power `n`.
+    /// # Errors
+    /// If the result is NaN.
+    pub fn try_powf(self, n: impl IntoInner<F>) -> Result<Self, NanError> {
+        self.0.try_powf(n.into_inner()).map(Self)
+    }
+    /// Attempts to raise `self` to the power `n`.
+    /// # Errors
+    /// If the result is NaN.
+    pub fn try_powi(self, n: i32) -> Result<Self, NanError> {
+        self.0.try_powi(n).map(Self)
+    }
+    #[track_caller]
+    #[must_use]
+    pub fn powf(self, n: impl IntoInner<F>) -> Self {
+        Self(self.0.powf(n.into_inner()))
+    }
+    #[track_caller]
+    #[must_use]
+    pub fn powi(self, n: i32) -> Self {
+        Self(self.0.powi(n))
+    }
+}
+
+use crate::Root;
+impl<F: IsNan + Root> Real<F> {
+    /// Attempts to find the square root of a number.
+    /// # Errors
+    /// If the result is NaN.
+    pub fn try_sqrt(self) -> Result<Self, NanError> {
+        self.0.try_sqrt().map(Self)
+    }
+    /// Attempts to find the cube root of a number.
+    /// # Errors
+    /// If the result is NaN.
+    pub fn try_cbrt(self) -> Result<Self, NanError> {
+        self.0.try_cbrt().map(Self)
+    }
+    #[track_caller]
+    #[must_use]
+    pub fn sqrt(self) -> Self {
+        Self(self.0.sqrt())
+    }
+    #[track_caller]
+    #[must_use]
+    pub fn cbrt(self) -> Self {
+        Self(self.0.cbrt())
+    }
+}
+
+use crate::Log;
+impl<F: IsNan + Log> Real<F> {
+    /// Attempts to find the log base `b` of self.
+    /// # Errors
+    /// If the result is NaN.
+    pub fn try_log(self, b: impl IntoInner<F>) -> Result<Self, NanError> {
+        self.0.try_log(b.into_inner()).map(Self)
+    }
+    /// Attempts to find the natural log (base e) of self.
+    /// # Errors
+    /// If the result is NaN.
+    pub fn try_ln(self) -> Result<Self, NanError> {
+        self.0.try_ln().map(Self)
+    }
+    /// Attempts to find the log base 2 of self.
+    /// # Errors
+    /// If the result is NaN.
+    pub fn try_log2(self) -> Result<Self, NanError> {
+        self.0.try_log2().map(Self)
+    }
+    /// Attempts to find the log base 10 of self.
+    /// # Errors
+    /// If the result is NaN.
+    pub fn try_log10(self) -> Result<Self, NanError> {
+        self.0.try_log10().map(Self)
+    }
+
+    #[track_caller]
+    #[must_use]
+    pub fn log(self, base: impl IntoInner<F>) -> Self {
+        Self(self.0.log(base.into_inner()))
+    }
+    #[track_caller]
+    #[must_use]
+    pub fn ln(self) -> Self {
+        Self(self.0.ln())
+    }
+    #[track_caller]
+    #[must_use]
+    pub fn log2(self) -> Self {
+        Self(self.0.log2())
+    }
+    #[track_caller]
+    #[must_use]
+    pub fn log10(self) -> Self {
+        Self(self.0.log10())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! assert_err {
+        ($e: expr) => {
+            assert!(($e).is_err())
+        };
+    }
+
+    macro_rules! real {
+        ($f: expr) => {
+            Real::new($f)
+        };
+    }
+
+    #[test]
+    #[should_panic]
+    fn assert_new_nan() {
+        real!(f32::NAN);
+    }
+    #[test]
+    #[should_panic]
+    fn assert_new_nan2() {
+        real!(-f32::NAN);
+    }
+
+    #[test]
+    fn assert_nan() {
+        assert_err!(real!(f32::INFINITY).try_add(f32::NEG_INFINITY));
+        assert_err!(real!(f32::INFINITY).try_sub(f32::INFINITY));
+        assert_err!(real!(0.0f32).try_mul(f32::INFINITY));
+        assert_err!(real!(0.0f32).try_div(0.0));
+        assert_err!(real!(f32::INFINITY).try_rem(1.0));
+        assert_err!(real!(1.0f32).try_rem(0.0));
+
+        assert_err!(real!(-1.0f32).try_sqrt());
+
+        assert_err!(real!(-1.0f32).try_log(3.0));
+        assert_err!(real!(-1.0f32).try_ln());
+        assert_err!(real!(-1.0f32).try_log2());
+        assert_err!(real!(-1.0f32).try_log10());
+    }
+
+    #[test]
+    fn assert_ops() {
+        assert_eq!(real!(2.0f32) + 1.0, real!(3.0));
+        assert_eq!(real!(2.0f32) - 1.0, real!(1.0));
+        assert_eq!(real!(5.0f32) * 2.0, real!(10.0));
+        assert_eq!(real!(8.0f32) / 2.0, real!(4.0));
+        assert_eq!(-real!(1.0f32), real!(-1.0));
+
+        assert_eq!(real!(4.0f32).powf(3.5), real!(128.0));
+        assert_eq!(real!(2.0f32).powi(8), real!(256.0));
+        assert_eq!(real!(4.0f32).sqrt(), real!(2.0));
+        assert_eq!(real!(27.0f32).cbrt(), real!(3.0));
+        assert_eq!(real!(16.0f32).log(4.0), real!(2.0));
+        assert_eq!(real!(1.0f32).ln(), real!(0.0));
+        assert_eq!(real!(8.0f32).log2(), real!(3.0));
+        assert_eq!(real!(1000.0f32).log10(), real!(3.0));
+    }
+
+    #[test]
+    #[allow(clippy::bool_assert_comparison)]
+    #[allow(clippy::cmp_nan)]
+    fn assert_cmp_weird() {
+        assert!(real!(f32::NEG_INFINITY) < real!(-1.0));
+        assert!(real!(-1.0f32) < real!(0.0));
+
+        assert_eq!(real!(0.0f32), real!(0.0));
+        assert_eq!(real!(0.0f32), real!(-0.0));
+        assert_eq!(real!(-0.0f32), real!(0.0));
+        assert_eq!(real!(-0.0f32), real!(-0.0));
+
+        assert!(real!(0.0) < real!(1.0));
+        assert!(real!(1.0) < real!(f32::INFINITY));
+
+        assert_eq!(real!(1.0) < f32::NAN, false);
+        assert_eq!(real!(1.0) >= f32::NAN, false);
+    }
+}
